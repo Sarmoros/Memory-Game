cc2ca3d25650af5c273559abfa713a034c09138c
Sarahy M <sarahym0703@gmail.com>
2024-05-02 18:20:46 -0400

    added box flipping function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 const emojis = ["⭐", "⭐", "🌈", "🌈", "🌞", "🌞", "🌙", "🌙", "🌎", "🌎", "🦄", "🦄", "🌺", "🌺", "🥥", "🥥"];
 
-var shuf_emojis = emojis.sort(() => (Math.random() > 0.5 ) ? 2 : -1);
+var shuf_emojis = emojis.sort(() => (Math.random() > .5 ) ? 2 : -1);
 for (var i=0; i<emojis.length; i++) {
     let box = document.createElement('div');
     box.className = 'item';
     box.innerHTML = shuf_emojis[i];
 
     box.onclick = function() {
-        this.classList.add('flipped');
-    };
+        this.classList.add('flipped'); 
+        setTimeout(function() {
+            if (document.querySelectorAll('.flipped').length > 1){
+                if (document.querySelectorAll('.flipped')[0].innerHTML == document.querySelectorAll('.flipped')[1].innerHTML){
+                    document.querySelectorAll('.flipped')[0].classList.add('boxMatch')
+                    document.querySelectorAll('.flipped')[1].classList.add('boxMatch')
+
+                    document.querySelectorAll('.flipped')[1].classList.remove('flipped')
+                    document.querySelectorAll('.flipped')[0].classList.remove('flipped')
+                    
+                    if (document.querySelectorAll('.boxMatch').length == emojis.length){
+                        alert('You win!')
+                    } 
+                } else {
+                    document.querySelectorAll('.flipped')[1].classList.remove('flipped')
+                    document.querySelectorAll('.flipped')[0].classList.remove('flipped')
+                    }
+                }
+        },500)
+    }
 
     document.querySelector('.game').appendChild(box);
 }
