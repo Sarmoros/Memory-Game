3e7988b0f9afbe5440c1a5f98db2e45d4b42662e
Sarahy M <sarahym0703@gmail.com>
2024-05-02 17:28:27 -0400

    added the emojis and the randomization of the emojis in each square

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const emojis = ["⭐", "⭐", "🌈", "🌈", "🌞", "🌞", "🌙", "🌙", "🌎", "🌎", "🦄", "🦄", "🌺", "🌺", "🥥", "🥥"];
+
+var shuf_emojis = emojis.sort(() => (Math.random() > 0.5 ) ? 2 : -1);
+for (var i=0; i<emojis.length; i++) {
+    let box = document.createElement('div');
+    box.className = 'item';
+    box.innerHTML = shuf_emojis[i];
+
+    box.onclick = function() {
+        this.classList.add('flipped');
+    };
+
+    document.querySelector('.game').appendChild(box);
+}
